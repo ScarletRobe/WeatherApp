99528f132f8c5577b2d2567f5994b18ce1b0dc9e
Leonid Chernyakov <scrltrb@gmail.com>
2022-08-17 16:17:08 +0300

    Feat: basic exception handling

--- source/js/fetch-weather.js
+++ source/js/fetch-weather.js
@@ -8,26 +8,38 @@ export default class Weather {
     this.lang = lang;
   }
 
-  async searchByCity (city) {
-    if (city === this.city) {
-      return;
-    }
+  destroySpaces (text) {
+    return text.replace(/\s/g, '-');
+  }
 
-    return this.fetchWeather(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.API_KEY}&units=metric&lang=${this.lang}`);
+  async searchByCity (city) {
+    let data = await this.fetchWeather(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.API_KEY}&units=metric&lang=${this.lang}`);
+    if (data) {
+      return data;
+    }
+    city = this.destroySpaces(city);
+
+    data = await this.fetchWeather(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.API_KEY}&units=metric&lang=${this.lang}`);
+    if (data) {
+      return data;
+    }
   }
 
   async searchByCoord (lat, lon) {
-    if (lat === this.lat && lon === this.lon) {
-      return;
-    }
-
     return this.fetchWeather(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.API_KEY}&units=metric&lang=${this.lang}`);
   }
 
   async fetchWeather (URL) {
-    const response = await fetch(URL);
-    const json = await response.json();
+    try {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const json = await response.json();
 
-    return json;
+      return json;
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
