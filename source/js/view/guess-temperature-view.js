630458f637621b42811b641d0773bb533ff28f47
Leonid Chernyakov <scrltrb@gmail.com>
2022-10-22 01:14:27 +0300

    Refactor: improve template

--- source/js/view/guess-temperature-view.js
+++ source/js/view/guess-temperature-view.js
@@ -2,18 +2,20 @@ import AbstractView from './abstract-view.js';
 
 const getGuessTemperatureTemplate = (cityInfo) => (
   `<div class="guess">
-    <div class="guess__about">
-      <h1 class="guess__country">${cityInfo.countryName},</h1>
-      <br>
-      <h1 class="guess__city">${cityInfo.name}</h1>
+    <div class="guess__known-info">
+      <div class="guess__about">
+        <h1 class="guess__country">${cityInfo.countryName},</h1>
+        <br>
+        <h1 class="guess__city">${cityInfo.name}</h1>
+      </div>
+      <div class="guess__other-info">
+        <div class="guess__date">Date:</div>
+        <div class="guess__time">Current time:</div>
+      </div>
     </div>
     <form class="guess__input">
-      <input class="guess__search-bar" type="text"><span class="guess__search-bar-celsium">°C</span>
+      <input class="guess__search-bar" type="text" placeholder="Enter your answer"><span class="guess__search-bar-celsium">°C</span>
     </form>
-    <div class="guess__other-info">
-      <div class="guess__date">Date:</div>
-      <div class="guess__time">Current time:</div>
-    </div>
   </div>`
 );
 
@@ -28,7 +30,7 @@ export default class GuessTemperatureView extends AbstractView {
     this.weatherInfo = weatherInfo;
     this.#cb = cb;
 
-    this.element.style.setProperty("--bg-url", `url('https://openweathermap.org/img/wn/${this.weatherInfo.weather[0].icon}@2x.png')`);
+    this.element.style.setProperty('--bg-url', `url('https://openweathermap.org/img/wn/${this.weatherInfo.weather[0].icon}@2x.png')`);
   }
 
   get template() {
