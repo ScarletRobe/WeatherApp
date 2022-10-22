7da6cc47fd9e5a06017b084e85c542ce3c5c539c
Leonid Chernyakov <scrltrb@gmail.com>
2022-10-22 18:00:01 +0300

    Feat: search image according to question

--- source/js/guess.js
+++ source/js/guess.js
@@ -1,5 +1,6 @@
 import Geonames from './fetch/fetch-geonames.js';
 import Weather from './fetch/fetch-weather.js';
+import Unsplash from './fetch/fetch-unsplash.js';
 
 import GuessTemperatureView from './view/guess/guess-temperature-view.js';
 import GuessResultView from './view/guess/guess-result-view.js';
@@ -9,6 +10,7 @@ import { GuessMode, GuessComponents } from './const.js';
 
 const geonames = new Geonames();
 const weather = new Weather();
+const unsplash = new Unsplash();
 
 // Элементы DOM
 
@@ -37,12 +39,16 @@ const renderGuessComponent = (payload) => {
   switch (payload.component) {
     case (GuessComponents.TEMPERATURE):
       prevComponent = guessTemperatureComponent;
-      guessTemperatureComponent = new GuessTemperatureView(cityInfo, weatherInfo, guessFormSubmitHandler);
+      guessTemperatureComponent = new GuessTemperatureView(cityInfo, weatherInfo);
+      guessTemperatureComponent.setSubmitHandler(guessFormSubmitHandler);
+
       component = guessTemperatureComponent;
       break;
     case (GuessComponents.RESULT):
       prevComponent = guessResultComponent;
       guessResultComponent = new GuessResultView(cityInfo, weatherInfo, payload.results);
+      guessResultComponent.setNextBtnClickHandler(guessNextBtnHandler);
+
       component = guessResultComponent;
       break;
   }
@@ -56,7 +62,6 @@ const renderGuessComponent = (payload) => {
 
   switch (currentMode) {
     case (GuessMode.QUESTION):
-      guessTemperatureComponent.setSubmitHandler();
       break;
   }
 
@@ -75,6 +80,7 @@ const removeGuessComponent = (component) => {
       break;
     case GuessComponents.RESULT:
       if (guessResultComponent) {
+        guessResultComponent.removeDocumentKeydownHandler();
         container.removeChild(guessResultComponent.element);
         guessResultComponent.removeElement();
         guessResultComponent = null;
@@ -94,14 +100,37 @@ const showGuessResult = (results) => {
     results,
   });
   guessResultComponent.manageBtnVisibility(true);
+  guessResultComponent.setDocumentKeydownHandler(guessNextBtnHandler);
+};
+
+const updateBackground = (images, city) => {
+  if (images) {
+    document.body.style.background = `url(${images.results[0].urls.full})`;
+  } else {
+    document.body.style.background = `url(https://source.unsplash.com/1600x900/?${city})`;
+  }
 };
 
 const initGuessComponent = async () => {
-  cityInfo = (await geonames.getCityInfo()).geonames[0];
-  weatherInfo = await weather.searchByCity(cityInfo.name);
-  renderGuessComponent({
-    component: GuessComponents.TEMPERATURE,
-  });
+  try {
+    cityInfo = (await geonames.getCityInfo()).geonames[0];
+    const [data, images] = await Promise.allSettled([
+      weather.searchByCity(cityInfo.name),
+      unsplash.getImage(cityInfo.name, cityInfo.countryName),
+    ]);
+
+    if (!data.value) {
+      throw new Error('Ошибка при загрузке информации о погоде');
+    }
+    weatherInfo = data.value;
+    renderGuessComponent({
+      component: GuessComponents.TEMPERATURE,
+    });
+    updateBackground(images.value, cityInfo.name);
+
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 function guessFormSubmitHandler(inputValue) {
@@ -111,6 +140,11 @@ function guessFormSubmitHandler(inputValue) {
     user: Number(inputValue),
     diff: Number(inputValue) - Number(Math.round(weatherInfo.main.temp)),
   });
+}
+
+function guessNextBtnHandler() {
+  removeGuessComponent();
+  initGuessComponent();
 }
 
 export {
