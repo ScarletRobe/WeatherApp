13be140a152b91a3ed7d60df93162f98f3b58c5d
Leonid Chernyakov <scrltrb@gmail.com>
2022-10-26 18:33:23 +0300

    Feat: add scoreboard view

---
+++ source/js/view/guess/scoreboard-view.js
@@ -0,0 +1,41 @@
+import AbstractView from '../abstract-view.js';
+
+const getScoreboardTemplate = () => (
+  `<div class="scoreboard">
+  <h1 class="scoreboard__header">You scored: 25!</h1>
+  <h2 class="scoreboard__description vh">Your place in scoreboard: 1!</h2>
+  <div class="scoreboard__list-wrapper">
+    <ol class="scoreboard__list">
+      <li class="scoreboard__item">name</li>
+      <li class="scoreboard__item">name</li>
+      <li class="scoreboard__item">name</li>
+      <li class="scoreboard__item">name</li>
+      <li class="scoreboard__item">name</li>
+    </ol>
+  </div>
+  <div class="scoreboard__registration">
+    <h2 class="scoreboard__registration-header">Enter your name to update scoreboard</h2>
+    <form action="" class="scoreboard__form">
+      <input class="guess__search-bar scoreboard__search-bar" type="text" placeholder="Enter your name here">
+      <button class="scoreboard__form-submit-btn" type="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" width="40px" height="40px"><defs><clipPath><path fill="#000" fill-opacity=".514" d="m-7 1024.36h34v34h-34z"/></clipPath><clipPath><path fill="#aade87" fill-opacity=".472" d="m-6 1028.36h32v32h-32z"/></clipPath></defs><path d="m345.44 248.29l-194.29 194.28c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744l171.91-171.91-171.91-171.9c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.29 194.28c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373" transform="matrix(.03541-.00013.00013.03541 2.98 3.02)" fill="rgba(255, 255, 255, 0.5)"/></svg>
+      </button>
+    </form>
+  </div>
+  <button class="scoreboard__try-again-btn">Try again</button>
+</div>`
+);
+
+export default class ScoreboardView extends AbstractView {
+  get template() {
+    return getScoreboardTemplate();
+  }
+
+  setSubmitHandler(cb) {
+    this.element.querySelector('.scoreboard__form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      cb(this.element.querySelector('.scoreboard__search-bar').value);
+      this.element.querySelector('.guess__search-bar').value = '';
+    });
+  }
+}
